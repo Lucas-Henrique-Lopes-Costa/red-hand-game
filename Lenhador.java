22970706638dd6dd400457b742f6cb20e0a2e816
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-10-27 10:15:57 -0300

    base do game nosso

---
+++ Lenhador.java
@@ -0,0 +1,31 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lenhador here.
+ * 
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lenhador extends Actor {
+    /**
+     * Act - do whatever the Lenhador wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Lenhador() {
+        getImage().scale(200, 200);
+    }
+    
+    public void act() {
+        if (Greenfoot.isKeyDown("left")) {
+            setImage("lenhador.jpeg");
+            getImage().mirrorHorizontally();
+            getImage().scale(200, 200);
+            // Greenfoot.playSound("corte.wav");
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            setImage("lenhador.jpeg");
+            getImage().scale(200, 200);
+            // Greenfoot.playSound("corte.wav");
+        }
+    }
+}
--- MeuMundo.java
+++ MeuMundo.java
@@ -12,7 +12,7 @@ public class MeuMundo extends World
     {    
         // Cria um novo mundo com 800x600 células,
         // com células de tamanho 1x1 pixels.
-        super(800, 600, 1); 
+        super(1200, 700, 1); 
         // Cria uma imagem do tamanho do mundo
         GreenfootImage imagemFundoPreto = new GreenfootImage(800, 600);
         // Desenha um retângulo preto em toda a imagem
@@ -20,5 +20,32 @@ public class MeuMundo extends World
         imagemFundoPreto.fillRect(0, 0, 800, 600);
         // Define a imagem com o plano de fundo do mundo
         setBackground(imagemFundoPreto);
+        prepare();
+    }
+    
+    /**
+     * Prepara o mundo para o início do programa.
+     * Ou seja: criar os objetos iniciais e adicioná-los ao mundo.
+     */
+    private void prepare()
+    {
+        Lenhador lenhador = new Lenhador();
+        addObject(lenhador,352,487);
+        lenhador.setLocation(351,511);
+        Lenhador lenhador2 = new Lenhador();
+        addObject(lenhador2,351,511);
+        lenhador.setLocation(431,433);
+        Lenhador lenhador3 = new Lenhador();
+        addObject(lenhador3,431,433);
+        lenhador.setLocation(431,433);
+        lenhador.setLocation(445,411);
+        removeObject(lenhador3);
+        lenhador.setLocation(383,544);
+        removeObject(lenhador2);
+        lenhador.setLocation(532,576);
+        lenhador.setLocation(412,623);
+        lenhador.setLocation(408,606);
+        lenhador.setLocation(752,536);
+        lenhador.setLocation(822,586);
     }
 }
