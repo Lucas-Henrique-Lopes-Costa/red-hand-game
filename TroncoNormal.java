1e012be57a31fcb8258a93d3919a84d41d3b2b87
Pedro Goncalves <pedrogoncalvescm@gmail.com>
2023-10-27 22:16:06 -0300

    Funciona so de um lado

--- TroncoNormal.java
+++ TroncoNormal.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Write a description of class TroncoNormal here.
@@ -8,12 +9,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TroncoNormal extends Actor
 {
+    
     /**
      * Act - do whatever the TroncoNormal wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public TroncoNormal ()
+    {
+
+    }
+    
+    public void addNaLista(TroncoNormal tronco)
+    {
+        
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
