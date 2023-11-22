db5e3f8d8ea88600da4b8f6150d49547ad5136be
Pedro Goncalves <pedrogoncalvescm@gmail.com>
2023-11-22 12:07:13 -0300

    Animação do Lenhador

--- Arvore.java
+++ Arvore.java
@@ -7,47 +7,74 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Arvore  
+public class Arvore
 {
     // instance variables - replace the example below with your own
-    private static ArrayList<TroncoNormal> listaTroncos = new ArrayList<>();
+    private  ArrayList<TroncoNormal> listaTroncos = new ArrayList<>();
+    private World mundo;
     /**
      * Constructor for objects of class Arvore
      */
-    public Arvore()
+    public Arvore(World mundo)
     {
+        this.mundo=mundo;
     }
     
-    public static void addTronco(TroncoNormal tronco)
+    public void addTronco(TroncoNormal tronco)
     {
         listaTroncos.add(tronco);
     }
     
-    public static TroncoNormal getTronco(int id)
+    public TroncoNormal getTronco(int id)
     {
         return listaTroncos.get(id);
     }
     
-    public static void removeTronco(int id)
+    public void removeTronco(int id)
     {
         listaTroncos.remove(id);
     }
     
-    public static int tamanho()
+    public int tamanho()
     {
         return listaTroncos.size();
     }
     
-    public static void limpa()
+    public void limpa()
     {
         listaTroncos.clear();
     }
     
-    public static void cair()
+    public void cair()
     {
         for(TroncoNormal tronco : listaTroncos)
         {
             tronco.cair160();
+        }
+    }
+    
+    
+    public void criaTronco()
+    {
+        int random = Greenfoot.getRandomNumber(11); // Gera um número aleatório entre 0 e 10
+        
+        if (random < 2) 
+        { // 20% de chance de cair um tronco com galho para a esquerda
+            TroncoNormal tronco = new TroncoNormal("esquerda");
+            listaTroncos.add(tronco);
+            mundo.addObject(tronco,314,-40);
+        } 
+        else if (random > 8) 
+        { // 20% de chance de cair um tronco com galho para a direita
+            TroncoNormal tronco = new TroncoNormal("direita");
+            listaTroncos.add(tronco);
+            mundo.addObject(tronco,439,-40);
+        } 
+        else 
+        { // 60% de chance de cair um tronco normal
+            TroncoNormal tronco = new TroncoNormal("normal");
+            listaTroncos.add(tronco);
+            mundo.addObject(tronco,377,-40);
         }
     }
 }
